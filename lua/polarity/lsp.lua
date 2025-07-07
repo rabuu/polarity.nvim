de9e86936d54af50c2f460892598aa73faa054ae
Rasmus Buurman <rasmus@rbuurman.de>
2025-07-07 16:08:48 +0200

    Support blink.cmp integration

--- lua/polarity/lsp.lua
+++ lua/polarity/lsp.lua
@@ -7,6 +7,8 @@ function M.setup(opts)
 	local capabilities = vim.lsp.protocol.make_client_capabilities()
 	if pcall(require, "cmp_nvim_lsp") then
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+	elseif pcall(require, "blink.cmp") then
+		capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities({}, false))
 	end
 
 	configs.polarity = {
